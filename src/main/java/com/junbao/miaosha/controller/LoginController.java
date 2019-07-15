72cc43253381528902e8e1f6d7f8e033461b71ea
ganfengbao <ganfengbao@gmail.com>
2019-07-16 07:00:13 +0800

    登录页面提交

--- src/main/java/com/junbao/miaosha/controller/LoginController.java
+++ src/main/java/com/junbao/miaosha/controller/LoginController.java
@@ -1,20 +1,28 @@
 package com.junbao.miaosha.controller;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.junbao.miaosha.result.Result;
+import com.junbao.miaosha.service.MiaoshaUserService;
 import com.junbao.miaosha.service.RedisService;
+import com.junbao.miaosha.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @author 小笨蛋
- */
+
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
 	private static Logger log = LoggerFactory.getLogger(LoginController.class);
+	
+	@Autowired
+    MiaoshaUserService userService;
 	
 	@Autowired
     RedisService redisService;
@@ -23,5 +31,13 @@ public class LoginController {
     public String toLogin() {
         return "login";
     }
-
+    
+    @RequestMapping("/do_login")
+    @ResponseBody
+    public Result<Boolean> doLogin(HttpServletResponse response, @Valid LoginVo loginVo) {
+    	log.info(loginVo.toString());
+    	//登录
+    	userService.login(response, loginVo);
+    	return Result.success(true);
+    }
 }
